6a3cf7b53fab30721b5c49257e158017b624534b
Thiago Miranda <thiagomwd@gmail.com>
2020-11-05 12:38:11 -0300

    Update server port

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,10 @@ import express from 'express';
 import routes from './routes';
 
 const app = express();
-
+const port = process.env.PORT || 8001;
 app.use(express.json());
 app.use(routes);
 
 
 
-app.listen(3333);
+app.listen(port);
