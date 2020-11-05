a06c6ea0c2e03b96bde50efb1023ec9d42767ef3
Thiago Miranda <thiagomwd@gmail.com>
2020-11-05 13:54:48 -0300

    Add a route for pipedrive webhook

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import DealsController from './controllers/DealsController';
 const routes = Router();
 
 routes.get('/sync', DealsController.syncDeals);
+routes.post('/hook', DealsController.syncDeals);
 routes.get('/deals', DealsController.getAggregation);
 routes.post('/deals', DealsController.create);
 
